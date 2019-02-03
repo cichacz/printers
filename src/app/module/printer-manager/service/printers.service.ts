2ee923cef2ab0078e65e18b54775761881725cc4
Cichacz <m.cichacz@gmail.com>
2019-02-03 18:28:18 +0100

    Fix SW

--- src/app/module/printer-manager/service/printers.service.ts
+++ src/app/module/printer-manager/service/printers.service.ts
@@ -1,24 +1,26 @@
-import { Injectable } from '@angular/core';
+import {ApplicationRef, Injectable} from '@angular/core';
 import {environment} from '../../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
 import {Printer} from '@app/module/printer-manager/model/printer';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {first, map} from 'rxjs/operators';
 
 @Injectable()
 export class PrintersService implements Resolve<Printer[]> {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private applicationRef: ApplicationRef) { }
 
   getPrinters() {
     return this.http.get<Printer[]>(environment.printersFile).pipe(
       map(list => {
-        return list.map(item => {
-          const printer = new Printer(item);
-          printer.enableLogging();
-          return printer;
+        const printers = list.map(item => new Printer(item));
+
+        this.applicationRef.isStable.pipe(first(stable => !!stable)).subscribe(() => {
+          printers.forEach(printer => printer.enableLogging());
         });
+
+        return printers;
       })
     );
   }
